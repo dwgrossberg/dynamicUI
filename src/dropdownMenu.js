1c1fb30e4aa301853435a1f9f4c47445af297217
Dan Grossberg <dwgrossberg@gmail.com>
2022-05-25 21:22:20 +0700

    Setup dropdown menu basic layout and module scope

--- src/dropdownMenu.js
+++ src/dropdownMenu.js
@@ -1,22 +1,62 @@
+import arrow from "./assets/arrow-white.png";
+
 const dropdownMenu = (() => {
   const dropdownMenuDOM = document.getElementById("dropdown-menu");
+
   const ulDOM = document.createElement("ul");
+  ulDOM.style.display = "flex";
+  ulDOM.style.justifyContent = "space-around";
+  ulDOM.style.listStyle = "none";
+  ulDOM.style.height = "100%";
 
   const setColumns = (...args) => {
-    // const menuDiv = document.createElement("div");
     args.forEach((arg) => {
       console.log(arg);
-      const argLink = document.createElement("li");
-      argLink.classList.add("nav");
-      argLink.innerText = arg;
-      ulDOM.appendChild(argLink);
+      const argLI = document.createElement("li");
+      argLI.classList.add("dropdown-nav");
+      argLI.style.display = "flex";
+      argLI.style.alignSelf = "center";
+      argLI.style.justifyContent = "center";
+      argLI.style.alignItems = "center";
+      argLI.style.width = "100%";
+      argLI.style.height = "100%";
+      argLI.style.cursor = "pointer";
+
+      const argA = document.createElement("a");
+      argA.style.textDecoration = "none";
+      argA.innerText = arg[0];
+      argA.href = arg[1];
+      argLI.appendChild(argA);
+      ulDOM.appendChild(argLI);
     });
+
     dropdownMenuDOM.appendChild(ulDOM);
   };
 
-  setColumns();
+  const setDropdown = (name, ...args) => {
+    const menuItems = Array.from(
+      document.getElementsByClassName("dropdown-nav")
+    );
+    const dropdown = () => {
+      args.forEach((arg) => {});
+    };
+    menuItems.forEach((item) => {
+      if (item.innerText === name) {
+        const arrowDiv = document.createElement("div");
+        arrowDiv.style.backgroundImage = `url(${arrow})`;
+        arrowDiv.style.backgroundSize = "100%";
+        arrowDiv.style.transform = "rotate(90deg)";
+        arrowDiv.style.marginLeft = "15px";
+        arrowDiv.style.height = "10px";
+        arrowDiv.style.width = "10px";
+        item.appendChild(arrowDiv);
 
-  const setDropdown = (index, ...args) => {};
+        item.addEventListener("mouseenter", dropdown);
+
+        console.log(args);
+      }
+    });
+  };
 
   return {
     setColumns,
--- src/index.js
+++ src/index.js
@@ -6,10 +6,19 @@ const faviconDOM = document.querySelector('link[rel~="icon"]');
 faviconDOM.href = favicon;
 
 dropdownMenu.setColumns(
-  "aaaaaaaaa",
-  "bbbbbbbbb",
-  "ccccccccc",
-  "ddddddddd",
-  "eeeeeeeeee",
-  "ffffffffff"
+  ["Home", "input href..."],
+  ["About", "input href..."],
+  ["Products", "input href..."],
+  ["Services", "input href..."],
+  ["History", "input href..."],
+  ["Contact", "input href..."]
+);
+
+dropdownMenu.setDropdown(
+  "Products",
+  "subLink-1",
+  "subLink-2",
+  "subLink-3",
+  "subLink-4",
+  "subLink-5"
 );
