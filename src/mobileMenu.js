5fa8effeaaadbaee18bd0b885942eb2c4272c23f
Dan Grossberg <dwgrossberg@gmail.com>
2022-06-02 16:34:21 +0700

    Rework mobile menu to incorporate a nav tag for css linking

--- src/mobileMenu.js
+++ src/mobileMenu.js
@@ -2,49 +2,46 @@ const mobileMenu = (() => {
   let mobileMenuDOM;
   const setMobileMenuDOM = (elemID) => {
     mobileMenuDOM = document.getElementById(`${elemID}`);
-    mobileMenuDOM.style.overflow = "hidden";
   };
 
-  const menuUL = document.createElement("ul");
-  menuUL.style.minWidth = "fit-content";
-  menuUL.style.listStyle = "none";
-  menuUL.style.boxSizing = "border-box";
-  menuUL.style.position = "relative";
-
   const setColumns = (...args) => {
+    const menuNav = document.createElement("nav");
+    menuNav.setAttribute("id", "menu");
+    const menuUL = document.createElement("ul");
+    menuUL.setAttribute("id", "menu-closed");
+    menuUL.style.boxSizing = "border-box";
+    menuUL.style.position = "relative";
     //  Add Close and More to the args list
-    [].push.call(args, [`Close &#215`, "#close-more"]);
-    [].push.call(args, [`More &#9776`, "#more"]);
+    [].push.call(args, [`&#215 Close`, "#menu-closed"]);
+    [].push.call(args, [`&#9776 More`, "#menu"]);
     const menuHeight = window.getComputedStyle(mobileMenuDOM).height;
+    menuNav.style.height = menuHeight;
+    menuNav.style.overflow = "hidden";
     args.forEach((arg) => {
       const argLI = document.createElement("li");
       argLI.classList.add("mobile-nav");
-      menuUL.style.maxHeight = menuHeight * 2;
+      //   menuUL.style.maxHeight = menuHeight * 2;
       argLI.style.cursor = "pointer";
-      argLI.style.display = "inline-block";
+      //   argLI.style.display = "inline-block";
       const argA = document.createElement("a");
       argA.setAttribute("id", `mobile-nav-link-${arg[0]}`);
       argA.innerHTML = arg[0];
       argA.href = arg[1];
-      argA.style.display = "inline-block";
-      argA.style.textDecoration = "none";
-      argA.style.textTransform = "uppercase";
-      argA.style.padding = "0 1em";
-      argA.style.letterSpacing = "0.1em";
-      argA.style.whiteSpace = "nowrap";
-      argA.style.lineHeight = menuHeight;
-      argA.style.height = menuHeight;
+      //   argA.style.display = "inline-block";
+      //   argA.style.textDecoration = "none";
+      //   argA.style.textTransform = "uppercase";
+      //   argA.style.padding = "0 1em";
+      //   argA.style.letterSpacing = "0.1em";
+      //   argA.style.whiteSpace = "nowrap";
+      //   argA.style.lineHeight = menuHeight;
+      //   argA.style.height = menuHeight;
       argLI.appendChild(argA);
       menuUL.appendChild(argLI);
+      menuNav.appendChild(menuUL);
     });
-    // Style the More and Close tabs
-    menuUL.lastChild.style.display = "absolute";
-    menuUL.lastChild.style.right = "0";
-    menuUL.lastChild.style.bottom = menuHeight;
-    menuUL.lastChild.style.paddingLeft = "3em";
     menuUL.childNodes[menuUL.childNodes.length - 2].style.display = "none";
 
-    mobileMenuDOM.appendChild(menuUL);
+    mobileMenuDOM.appendChild(menuNav);
   };
 
   return {
